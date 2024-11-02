f453698b46b6fb98036edfc469941f89006ae75c
lunarmax01 <lunargold777@gmail.com>
2024-11-03 00:51:04 +0500

    list

--- src/page/movies-list.jsx
+++ src/page/movies-list.jsx
@@ -1,15 +1,22 @@
+import { SiGoogledatastudio } from "react-icons/si";
+
+
+
 function Movies_list() {
     return ( <>
-    
-        <div  className="">
-
-                <div>
-                        <div>
+                <div id="listbg" className=" w-full h-auto">
+                <div className="w-full border-white/0 border-[5px]"></div>
+                <marquee className='w-full h-auto flex items-center' behavior="" direction="">
+                    <div id="list" className="w-72 h-40 rounded-2xl group">
+                        <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                                <h1 className=" group-hover:text-[#5bff58] text-black/0 font-serif"><b className="text-black/0  group-hover:text-black font-bold">BLACK </b>MOVIES</h1>
+                                <SiGoogledatastudio  className=" text-6 relative left-32 top-10 text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all"/>
 
                         </div>
+                    </div>
+                </marquee>
+                <div className="w-full border-white/0 border-[5px]"></div>
                 </div>
-
-        </div>
     
     </> );
 }
