018a97f4319fee96a9d76005bc3270d347a0b30f
lunarmax01 <lunargold777@gmail.com>
2024-11-04 14:02:26 +0500

    1 marquee full

--- src/page/movies-list.jsx
+++ src/page/movies-list.jsx
@@ -3,23 +3,108 @@ import { SiGoogledatastudio } from "react-icons/si";
 
 
 function Movies_list() {
-    return ( <>
-                <div id="listbg" className=" w-full h-auto">
-                <div className="w-full border-white/0 border-[5px]"></div>
-                <marquee className='w-full h-40vh flex items-center' behavior="" direction="">
-                    <div id="list" className="w-72 h-40 rounded-2xl group flex">
+    return (<>
+        <div id="listbg" className=" w-full h-auto">
+            <div className="w-full border-white/0 border-[5px]"></div>
+            <marquee id="mrq" className='w-full h-40vh flex items-center' behavior="scroll" scrolldelay="40" direction="">
+                <div id="listhom" className=" w-full h-auto flex">
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listwednesday" className="w-[400px] h-60 rounded-2xl group flex">
                         <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
-                                <h1 className=" group-hover:text-[#5bff58] text-black/0 font-serif"><b className="text-black/0  group-hover:text-black font-bold">BLACK </b>MOVIES</h1>
-                                <SiGoogledatastudio  className=" text-6 relative left-32 top-10 text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all"/>
-                                <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-9">2022-AQSH</h1>
-                                <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
                         </div>
                     </div>
-                </marquee>
-                <div className="w-full border-white/0 border-[5px]"></div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listvenom" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listironman" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listpantera" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listshangchi" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listBeetlejuice" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listavatar" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listhellboy" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listiztopar" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
+                    <div id="listmerlin" className="w-[400px] h-60 rounded-2xl group flex">
+                    <div className="w-full h-full group-hover:bg-black/40 px-2 py-2">
+                            <h1 className=" group-hover:text-white text-black/0 font-serif"><b className="text-black/0  group-hover:text-[#5bff58] font-bold">BLACK </b>MOVIES</h1>
+                            <SiGoogledatastudio className=" text-6 relative left-[190px] top-[75px] text-black/0 group-hover:text-[#5bff58] text-[25px] rotate-[180deg] scale-125 transition-all" />
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold mt-32">2022-AQSH</h1>
+                            <h1 className="text-black/0 group-hover:text-[#5bff58] font-bold">Detaktiv, Komediya...</h1>
+                        </div>
+                    </div>
+                    <div className="h-40 border-[3px] border-white/0"></div>
                 </div>
-    
-    </> );
+            </marquee>
+            <div className="w-full border-white/0 border-[5px]"></div>
+        </div>
+
+    </>);
 }
 
 export default Movies_list;
