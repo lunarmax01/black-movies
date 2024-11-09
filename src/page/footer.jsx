00ccfd977afa1eefd7c21882aeb05309e92577e0
lunarmax01 <lunargold777@gmail.com>
2024-11-09 18:29:29 +0500

    footer full

--- src/page/footer.jsx
+++ src/page/footer.jsx
@@ -1,25 +1,132 @@
-function Footer() {
-    return ( <>
-    
-            <footer className="bg-Black text-white py-6 px-4 ">
-                <div>
-                    {/* biz xaqimizda */}
-                    <div>
-                        <h3 className="text-white">Biz haqimizda</h3>
-                        
-                    </div>
-                    {/* platformalarimiz va dasturlarimiz*/}
-                    <div>
-                        
-                    </div>
-                    {/* version */}
-                    <div>
+import { FaApple, FaGooglePlay, FaTelegram, FaInstagramSquare, FaFacebookF, FaTwitter } from "react-icons/fa";
+import { SiShopify } from "react-icons/si";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from 'react';
 
+
+function Footer() {
+  const [drawerState, useDrawerState] = useState()
+
+  const toggleDrawerState = () => {
+    useDrawerState(!drawerState)
+  }
+  const [drawerState2, useDrawerState2] = useState()
+
+  const toggleDrawerState2 = () => {
+    useDrawerState2(!drawerState2)
+  }
+    return (<>
+
+        <footer className="bg-[#1D1F1E] text-white pt-6 px-4 h-auto">
+            <div className="w-full h-full  flex items-start justify-between">
+                <div className=" w-full h-auto flex-col xl:pr-10 xl:pl-4 md:pr-8 sm:pr-6 xl:h-0">
+                    <h3 onClick={toggleDrawerState} className="x:text-[18px] xs:text-2xl font-bold mb-1 flex border-b-[2px] cursor-pointer">Biz haqimizda</h3>
+                    <div className={`${drawerState ? 'block' : 'hidden '} mt-2`}>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">kontaktlar</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">Qo'llab -quvatlash</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">Obunalar</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">Hamkorlar</h1>
+                    </div>
+                    <h3 onClick={toggleDrawerState2} className="x:text-[18px] xs:text-2xl font-bold mb-1 flex border-b-[2px] cursor-pointer mt-4">Biz haqimizda</h3>
+                    <div className={`${drawerState2 ? 'block' : 'hidden '} mb-5 mt-4`}>
+                    <h1 className=" text-[#737474] hover:text-white/70 mt-1">TV</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">Kino</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">seriallar</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">multfilmlar</h1>
+                        <h1 className=" mt-1 text-[#737474] hover:text-white/70">Anime</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">Radio</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">UZ Video</h1>
+                        <h1 className=" text-[#737474] hover:text-white/70 mt-1">Sport</h1>
+                        <h1 className=" mt-1 text-[#737474] hover:text-white/70 ">Axborat</h1>
                     </div>
                 </div>
-            </footer>
-    
-    </> );
+                
+
+                {/* platformalarimiz va dasturlarimiz*/}
+                <div className=" w-auto">
+                    {/* datsurlari */}
+                    <div className="xl:w-[700px] xl:h-32 xl:flex xl:justify-center lg:justify-center 2xl:justify-between lg:w-[500px] lg:flex md:w-[500px] md:flex sm:hidden sm:w-0 sm:h-0 xs:hidden xs:w-0 xs:h-0 x:hidden x:w-0 x:h-0">
+                        <div className="w-[65%] md:w-[90%] xs:w-[100%] h-28 hover:*:bg-[#08180e] grid gap-y-4 gap-x-4 grid-cols-3  py-6 mt-10 md:mt-0">
+                            <h1 className=" items-center transition-all bg-[#292A29] text-lg py-4 px-2 w-30 flex rounded-xl"><FaApple className="mr-1 text-2xl" />App Store</h1>
+
+                            <h1 className=" items-center transition-all bg-[#292A29] text-md py-4 px-2 w-30 flex rounded-xl"><FaGooglePlay className="mr-1 text-2xl" />Google Play</h1>
+
+                            <h1 className=" items-center transition-all bg-[#292A29] text-md py-4 px-2 w-30 flex rounded-xl"><SiShopify className="mr-1 text-2xl" />Gallaxy store</h1>
+
+                        </div>
+                        {/* platforma */}
+                        <div className="2xl:w-[250px] 2xl:ml-2 2xl:flex 2xl:flex-col 2xl:items-center md:hidden md:w-0 md:h-0 xs:hidden xs:w-0 xs:h-0">
+                            <h1 className=" text-2xl font-bold">Ijtimoiy tarmoqlar</h1>
+                            <br />
+                            <div className="w-24 flex items-center flex-col mt-5">
+                                <div className="w-full flex justify-between mb-4 hover:*:bg-[#08180e]">
+                                    <div className="w-[45px] h-[45px] bg-[#292A29] flex items-center justify-center rounded-md">
+                                        <FaTelegram className=" text-[30px] text-white" />
+                                    </div>
+                                    <div className="w-[45px] h-[45px] bg-[#292A29] flex items-center justify-center rounded-md">
+                                        <FaInstagramSquare className=" text-[30px] text-white" />
+                                    </div>
+                                </div>
+                                <div className="w-full flex justify-between hover:*:bg-[#08180e]">
+                                    <div className="w-[45px] h-[45px] bg-[#292A29] flex items-center justify-center rounded-md">
+                                        <FaFacebookF className=" text-[30px] text-white" />
+                                    </div>
+                                    <div className="w-[45px] h-[45px] bg-[#292A29] flex items-center justify-center rounded-md">
+                                        <FaTwitter className=" text-[30px] text-white" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* version */}
+            </div>
+            {/*  */}
+            <div className="2xl:w-[700px] xl:w-80 xl:h-36 lg:h-[150px] flex xl:justify-center lg:justify-center 2xl:justify-between 2xl:hidden 2xl:h-0 lg:flex-col md:flex-col sm:flex-col xs:flex-col x:flex-col ">
+                <div className="xs:w-[70%] lg:w-[50%] sm:w-[73%] h-28 hover:*:bg-[#08180e] md:gap-y-4 md:gap-x-4 md:grid-cols-3 sm:grid sm:gap-y-4 sm:gap-x-4 sm:grid-cols-3 xs:flex-col  py-6 mt-10 xl:w-0 xl:hidden s:hidden s:w-0 s:h-0 md:mt-5 sm:mt-0 xs:mt-0  md:hidden md:w-0 md:h-0 sm:ml-0 xs:ml-0 x:ml-0 x:mt-0">
+                    <h1 className=" items-center transition-all bg-[#292A29] text-md py-4 px-2 w-[150px] xs:w-[130px] xs:text-[15px] flex rounded-xl xs:mt-2"><FaApple className="mr-1 text-2xl xs:text-[20px] " />App Store</h1>
+
+                    <h1 className=" items-center transition-all bg-[#292A29] text-md py-4 px-2 w-[150px] xs:w-[130px] xs:text-[15px] flex rounded-xl xs:mt-2 x:mt-2"><FaGooglePlay className="mr-1 text-2xl xs:text-[20px]" />Google Play</h1>
+
+                    <h1 className=" items-center transition-all bg-[#292A29] text-md py-4 px-2 w-[150px] xs:w-[130px] xs:text-[15px] flex rounded-xl xs:mt-2 x:mt-2"><SiShopify className="mr-1 text-2xl xs:text-[20px] " />Gallaxy store</h1>
+
+                </div>
+                {/* platforma */}
+                <div className="2xl:h-0 2xl:hidden xl:w-[250px] xl:flex-col lg:pl-2 xs:mt-32 lg:mt-0 md:mt-0 sm:mt-0 x:mt-28 s:pl-0 xl:pl-4 ">
+                    <h1 className=" text-2xl font-bold">Ijtimoiy tarmoqlar</h1>
+                    <br />
+                    <div className="w-28 flex xl:justify-center mt-1 xl:relative xl:ml-[89px] lg:relative lg:right-[89px]">
+                        <div className="w-full flex mb-4 lg:ml-20 hover:*:bg-[#08180e] ">
+                            <div className="w-[45px] h-[45px] ml-2 bg-[#292A29] flex items-center justify-center rounded-md">
+                                <FaTelegram className=" text-[30px] text-white" />
+                            </div>
+                            <div className="w-[45px] h-[45px] ml-2 bg-[#292A29] flex items-center justify-center rounded-md">
+                                <FaInstagramSquare className=" text-[30px] text-white" />
+                            </div>
+                        </div>
+                        <div className="w-full flex mb-6 hover:*:bg-[#08180e]">
+                            <div className="w-[45px] h-[45px] ml-2 bg-[#292A29] flex items-center justify-center rounded-md">
+                                <FaFacebookF className=" text-[30px] text-white" />
+                            </div>
+                            <div className="w-[45px] h-[45px] ml-2 bg-[#292A29] flex items-center justify-center rounded-md">
+                                <FaTwitter className=" text-[30px] text-white" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full h-20 py-5 px-4 lg:mt-16 flex justify-between border-t-[1px] border-[#7374744d] x:flex-col">
+                <h1>© 2024 - Black-movies (version).v01</h1>
+                <div className=" flex x:mt-2">
+                    <h1 className=" text-[#737474] hover:text-white/70 ml-4 cursor-pointer">Русский</h1>
+                    <h1 className=" text-[#737474] hover:text-white/70 ml-4 cursor-pointer">O'zbekcha</h1>
+                    <h1 className=" text-[#737474] hover:text-white/70 ml-4 cursor-pointer">English</h1>
+                </div>
+            </div>
+        </footer>
+
+    </>);
 }
 
 export default Footer;
--- src/page/navbar.jsx
+++ src/page/navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import { IoIosSearch } from "react-icons/io";
 import { CiMenuFries, CiCircleRemove } from "react-icons/ci";
-import { MdHome, MdMovieFilter, MdVolunteerActivism } from "react-icons/md";
+import { MdHome, MdMovieFilter, MdVolunteerActivism, MdLiveTv  } from "react-icons/md";
 import { SiSteelseries } from "react-icons/si";
 
 import { useState } from 'react';
@@ -18,34 +18,46 @@ const Navbar = () => {
   return (<>
     <div className='w-[100%] h-[70px] flex items-center xs:justify-around justify-between px-4 bg-slate-900 fixed top-0 left-0 z-10'>
       <Link to="/" className='text-white text-2xl x:text-xl md:text-2xl w-[150px]'><b className='text-green-500 md:w-0' >Black </b>Movies</Link>
-      <ul className=' lg:w-[30%] items-center justify-center sm:w-[50%] md:block hidden md:ml-5'>
-        <NavLink to="/" className="lg:py-2 lg:px-2 lg:ml-2 text-white hover:opacity-85 lg:text-[18px] sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Home</NavLink>
-        <NavLink to="/series" className="lg:py-2 lg:px-2 lg:ml-2 text-white hover:opacity-85 lg:text-[18px] sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">series</NavLink>
-        <NavLink to="/Movie" className="lg:py-2 lg:px-2 lg:ml-2  text-white hover:opacity-85 lg:text-[18px] sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Movie</NavLink>
-        <NavLink to="/Cartoon" className="lg:py-2 lg:px-2 lg:ml-2  text-white hover:opacity-85 lg:text-[18px]  sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Cartoon</NavLink>
+      <ul className=' lg:w-[34%] items-center justify-center sm:w-[50%] md:block hidden md:ml-5'>
+        <NavLink to="/" className="lg:py-2 lg:px-2 lg:ml-2 text-white hover:opacity-85 lg:text-[18px] sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Bosh sahifa</NavLink>
+        <NavLink to="/tv" className="lg:py-2 lg:px-2 lg:ml-2 text-white hover:opacity-85 lg:text-[18px] sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">tv</NavLink>
+        <NavLink to="/Movie" className="lg:py-2 lg:px-2 lg:ml-2  text-white hover:opacity-85 lg:text-[18px] sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Kino</NavLink>
+        <NavLink to="/series" className="lg:py-2 lg:px-2 lg:ml-2  text-white hover:opacity-85 lg:text-[18px]  sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Serial</NavLink>
+        <NavLink to="/multfilm" className="lg:py-2 lg:px-2 lg:ml-2  text-white hover:opacity-85 lg:text-[18px]  sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Multfilmlar</NavLink>
+        <NavLink to="/full" className="lg:py-2 lg:px-2 lg:ml-2  text-white hover:opacity-85 lg:text-[18px]  sm:text-[18px] sm:px-4 sm:ml-2 md:text-lg md:ml-0 md:px-2 md:mt-1">Boshqalar</NavLink>
       </ul>
       {/* search */}
       <div className='flex item-center sm:w-[300px] xs:w-[200px] w-[100px] h-[60%] border-[1px] border-gray-700 rounded-md'>
         <IoIosSearch className='text-white py-2 px-2 text-4xl xs:block hidden' />
         <input type="text" className='bg-transparent border-none outline-none xs:px-0 px-2 text-white w-[100%] lg:text-[15px] md:text-[15px]' placeholder='search' />
       </div>
-      <CiMenuFries className='lg:py-2 lg:px-2 lg:text-4xl lg:bg-green-500 lg:text-white lg:rounded-md sm:text-4xl md:text-[30px] md:ml-[10px] text-green-500 x:text-[22px]' onClick={toggleDrawerState} />
+      <CiMenuFries className='lg:py-2 lg:px-2 lg:text-4xl lg:bg-green-500 lg:text-white lg:rounded-md sm:text-4xl md:text-[30px] md:ml-[10px] text-green-500 x:text-[22px] cursor-pointer' onClick={toggleDrawerState} />
 
       <div className={`${drawerState ? 'block' : 'hidden '}`}>
         <div id='drawer' className='w-[300px] h-[100vh] fixed right-0 top-0 z-10 border-l-[2px]'>
           <div className='flex flex-col w-[100%] h-full items-center px-0'>
             <div className=' w-full flex justify-between mb-8'><h2 className='text-white font-bold text-3xl'>Drawer</h2>
-              <CiCircleRemove className='py-2 px-2 text-4xl text-white bg-slate-900 mt-2 rounded-md' onClick={toggleDrawerState} />
+              <CiCircleRemove className='py-2 px-2 text-4xl text-white bg-slate-900 mt-2 rounded-md cursor-pointer' onClick={toggleDrawerState} />
             </div>
             <div className='w-full border-[1px] border-[#ffffff7c]'></div>
-            <NavLink id='drewera' to='/' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdHome className='text-[30px] mr-2'/> Home</h1></NavLink>
+            <NavLink id='drewera' to='/' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdHome className='text-[30px] mr-2'/> Bosh sahifa</h1></NavLink>
+            
+            <div className='w-full border-[1px] border-[#ffffff7c]'></div>
+            <NavLink id='drewera' to='/Cartoon' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdLiveTv  className='text-[30px] mr-2'/> TV</h1></NavLink>
+
               <div className='w-full border-[1px] border-[#ffffff7c]'></div>
-            <NavLink id='drewera' to='/Series' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><SiSteelseries className='text-[30px] mr-2'/> Series</h1></NavLink>
+            <NavLink id='drewera' to='/Movie' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdMovieFilter className='text-[30px] mr-2'/> Kino</h1></NavLink>
+
             <div className='w-full border-[1px] border-[#ffffff7c]'></div>
-            <NavLink id='drewera' to='/Movie' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdMovieFilter className='text-[30px] mr-2'/> Movie</h1></NavLink>
+            <NavLink id='drewera' to='/Series' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><SiSteelseries className='text-[30px] mr-2'/> Serial</h1></NavLink>
+
             <div className='w-full border-[1px] border-[#ffffff7c]'></div>
-            <NavLink id='drewera' to='/Cartoon' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdVolunteerActivism className='text-[30px] mr-2'/> Cartoon</h1></NavLink>
+            <NavLink id='drewera' to='/Cartoon' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdVolunteerActivism className='text-[30px] mr-2'/> Multfilm</h1></NavLink>
+
             <div className='w-full border-[1px] border-[#ffffff7c]'></div>
+            <NavLink id='drewera' to='Anime' className='w-full h-10 flex bg-[#47556973] transition-all hover:bg-slate-600 hover:text-[#50ffa2] pl-3'><h1 className='flex items-center text-xl font-bold'><MdVolunteerActivism className='text-[30px] mr-2'/> Anime</h1></NavLink>
+            <div className='w-full border-[1px] border-[#ffffff7c]'></div>
+            
           </div>
         </div>
       </div>
